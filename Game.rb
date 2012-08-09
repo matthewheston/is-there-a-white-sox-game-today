4fc3d44e56f05c5cd85a8658129e917e5c8058be
Matthew Heston <matthewheston@gmail.com>
2012-08-09 18:31:48 -0500

    Extract today? method into a new Game module.

---
+++ Game.rb
@@ -0,0 +1,15 @@
+module Game
+
+  def self.set_games(games)
+    @@games = games
+  end
+
+  def self.today?
+    @@games.find do |game|
+      game.dtstart.new_offset(DateTime.now.offset).year == DateTime.now.year and
+      game.dtstart.new_offset(DateTime.now.offset).month == DateTime.now.month and
+      game.dtstart.new_offset(DateTime.now.offset).day == DateTime.now.day
+    end
+  end
+
+end
--- sox.rb
+++ sox.rb
@@ -1,24 +1,17 @@
 require 'sinatra'
 require 'icalendar'
+require './Game.rb'
 
 configure do
   cal_file = File.open("res/schedule.ics")
   cal = Icalendar.parse(cal_file).first
-  @@games = cal.events.find_all { |e| e.dtstart > DateTime.now }
+  Game.set_games( cal.events.find_all { |e| e.dtstart > DateTime.now })
 end
 
 get '/' do
-  if game_today?
+  if Game.today?
       "There is a game today."
     else
       "There is not a game today"
     end
-end
-
-def game_today?
-  @@games.find do |game|
-    game.dtstart.new_offset(DateTime.now.offset).year == DateTime.now.year and
-    game.dtstart.new_offset(DateTime.now.offset).month == DateTime.now.month and
-    game.dtstart.new_offset(DateTime.now.offset).day == DateTime.now.day
-  end
 end
