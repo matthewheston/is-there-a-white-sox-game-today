bcb0b5057d9b5fa0838037495bc65b14c47cfacf
Matthew Heston <matthewheston@gmail.com>
2012-09-21 09:34:44 -0500

    Use rical. Add index view.

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ gem 'mocha', :require => false
 
 source :rubyforge
 gem 'icalendar'
+gem 'ri_cal'
+gem 'tzinfo'
+gem 'activesupport', :require => "active_support/time"
--- sox.rb
+++ sox.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
 require 'icalendar'
+require 'ri_cal'
 require './Game.rb'
 
 configure do
-  cal_file = File.open("res/schedule.ics")
-  cal = Icalendar.parse(cal_file).first
+  cal_file = File.read("res/schedule.ics")
+  cal = RiCal.parse_string(cal_file).first
   Game.set_games( cal.events.find_all { |e| e.dtstart > DateTime.now })
 end
 
